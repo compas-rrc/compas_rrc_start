b378a9df7d46e3f4e5c9b22a00cfba1d56816881
fleischp <fleischmann@arch.ethz.ch>
2025-11-10 16:25:09 +0100

    EDIT: draft of new custom instruction template

--- Python/instruction-examples/utility/custom_instruction.py
+++ Python/instruction-examples/utility/custom_instruction.py
@@ -10,17 +10,38 @@ if __name__ == '__main__':
     abb = rrc.AbbClient(ros, '/rob1')
     print('Connected.')
 
-    # Custom instruction name (RAPID Procedure name)
-    instruction = 'r_RRC_CustomInstruction'
+    # ========================================================================================================================
+    # Call of CustomInstruction
+    # ========================================================================================================================
+    #
+    # 'r_RRC_CustomInstruction': Name of the RAPID procedure to be called.
+    #                            'r_' is the recommended prefix for routines, and '_RRC_' 
+    #                            is the reserved key for official RRC commands/functions in RAPID.
+    #                            For your own custom instructions, use your own key such as '_ABC_' 
+    #                            (3–4 characters recommended).
+    #
+    # ['Text1','Text2']:         Example showing how to pass up to 8 strings (each up to 80 characters) from Python to RAPID.
+    #
+    # [1.1,2.2]:                 Example showing how to pass up to 36 numeric values from Python to RAPID.
+    #
+    # ========================================================================================================================
 
-    # string vlaue list of 8 strings 
-    string_values = ['Mytext Text']
+    raw_debug_output = abb.send_and_wait(rrc.Debug(rrc.CustomInstruction('r_RRC_CustomInstruction', ['Python value 1: ','Python value 2: '], [1.11,2.22])))
 
-    # float value list of 36 floats 
-    float_values = [42]
+    rapid_string_1 = raw_debug_output['string_values'][0]
+    rapid_string_2 = raw_debug_output['string_values'][1]
 
-    # Custom instruction 
-    done = abb.send_and_wait(rrc.CustomInstruction(instruction, string_values, float_values))
+    rapid_value_1 = round(raw_debug_output['float_values'][0],2)
+    rapid_value_2 = round(raw_debug_output['float_values'][1],2)
+
+    print(rapid_string_1, rapid_value_1)
+    print(rapid_string_2, rapid_value_2)
+
+    # End of CustomInstruction example
+    # ========================================================================================================================
+
+    # No operation 
+    done = abb.send_and_wait(rrc.Noop())
 
     # Print feedback 
     print('Feedback = ', done)
